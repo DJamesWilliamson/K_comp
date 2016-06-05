b4e38b3a3cb07a316ef677704ad94bdb9f7445a7
DJamesWilliamson <jamesandshantha@yahoo.com.au>
2016-06-06 00:39:23 +1000

    imputation procedure and data sets

--- imputation.R
+++ imputation.R
@@ -1,6 +1,210 @@
 #################################################################################
-# Import training set but replace blanks with NA
-trainMi <- read.csv("train2016.csv", na.strings = c("", "NA"))
+# Imputation 1: uses data from both training and test datasets, no additional feature
+
+# Import training and test sets but replace blanks with NA
+train2016 <- read.csv("train2016.csv", na.strings = c("", "NA"))
+test2016 <- read.csv("test2016.csv", na.strings = c("", "NA"))
+str(train2016)
+str(test2016)
+names(train2016)[1:8]
+names(test2016)[1:8]
+
+# Remove the ID and response variables from train2016
+trainMi <- train2016[ , -c(1, 7)]
+testMi <- test2016[ , -c(1)]
+names(trainMi)[1:8]
+names(testMi)[1:8]
+
+# Combine train and test datasets (and remove others from workspace)
+impData <- rbind(trainMi, testMi)
+rm(train2016, test2016, trainMi, testMi)
+
+summary(impData)
+
+# Set to NA cases with  10 < age < 100 (for later imputation)
+impData$YOB[which(2013 - impData$YOB > 100)] <- NA
+impData$YOB[which(2013 - impData$YOB < 11)] <- NA
+
+# Replace YOB with factor variable in deciles of YOB; plans to reduce errors
+impData$YOB = cut(impData$YOB, quantile(impData$YOB, prob=seq(0, 1, length=11), 
+                                        na.rm = TRUE, type = 5), ordered_result = TRUE)
+
+# Order other factors
+impData$Income <- ordered(impData$Income,
+                          levels = c("under $25,000", "$25,001 - $50,000",
+                                     "$50,000 - $74,999", "$75,000 - $100,000",
+                                     "$100,001 - $150,000", "over $150,000"))
+impData$EducationLevel <- ordered(impData$EducationLevel,
+                                  levels = c("Current K-12", "High School Diploma",
+                                             "Current Undergraduate", "Associate's Degree",
+                                             "Bachelor's Degree", "Master's Degree",
+                                             "Doctoral Degree"))
+str(impData)
+################################################################################
+# Imputation preparation
+library(mice)
+library(VIM)
+md.pattern(impData)
+
+# tail(md.pattern(impData)[ ,1:8])
+# marginplot(impData[, c("EducationLevel", "Income")], col = mdc(1:2), cex = 1.2,
+#            cex.lab = 1.2, cex.numbers = 1.3, pch = 19)
+
+# Proportion of usable cases
+p <- md.pairs(impData)
+use_cases <- round(p$mr/(p$mr + p$mm), 3)
+head(use_cases)
+class(use_cases)
+mean(use_cases < 0.1)
+
+# Create default prediction matrix and examine the distribution of variables used
+pred_quick <- quickpred(impData)
+hist(rowSums(pred_quick))
+
+# Export pred_quick
+write.csv(as.data.frame(pred_quick), file = "pred_quick_imp1.csv")
+
+# This is combined with the question matrix I derived from the question selection
+# procedure so that the non-Q predictions come from here and the Q prediction matrix
+# comes from the question selections
+
+# Import the combined prediction data and remove the first column, name rows, create matrix
+pred <- read.csv("pred_quick_imp1_q_comb.csv", header = TRUE, stringsAsFactors = FALSE)
+pred <- pred[ , -c(1)]
+colnames(pred)
+rownames(pred) <- colnames(pred)
+pred <- as.matrix(pred)
+
+# Questions not used for imputation 1
+which(colSums(pred) == 0)
+# Do you find it easier to start and maintain a new good habit, or to permanently kick a bad habit?
+# Is your alarm clock intentionally set to be a few minutes fast?
+# Are you left-handed?
+
+# Number or predictor variables used for each imputation
+hist(rowSums(pred))
+which(rowSums(pred) < 10)       # These are the ones (37 and 94) from before
+# Do you find it easier to start and maintain a new good habit, or to permanently kick a bad habit?
+# Are you left-handed?
+# Could add the predictors from pred_quick to the 6 I identified, but not worth it
+
+################################################################################
+# Trial imputation 1 (~5 mins)
+imp1 <- mice(impData, pred = pred, seed = 23109)
+
+# Check the prediction matrix and sets
+print(imp1)
+imp1$imp$YOB             # some variability in the imputations between runs
+head(complete(imp1))     # look at first complete set
+head(complete(imp1, 2))  # look at the others complete set
+
+imp1_set1 <- complete(imp1)
+imp1_set2 <- complete(imp1, 2)
+imp1_set3 <- complete(imp1, 3)
+imp1_set4 <- complete(imp1, 4)
+imp1_set5 <- complete(imp1, 5)
+
+write.csv(imp1_set1, "imp1_set1.csv")
+write.csv(imp1_set2, "imp1_set2.csv")
+write.csv(imp1_set3, "imp1_set3.csv")
+write.csv(imp1_set4, "imp1_set4.csv")
+write.csv(imp1_set5, "imp1_set5.csv")
+
+par(mfrow = c(3, 1))
+plot(imp1, c("YOB", "Gender", "Income", "HouseholdStatus", "EducationLevel"))
+plot(imp1, c(names(colnames(pred)[5:length(colnames(pred))])))
+par(mfrow = c(1, 1))
+# May be some problems with convergence with Q102687 and Q108856; others not ideal
+# Do you eat breakfast every day?
+# Lots of people are around! Are you more likely to be right in the middle of things, 
+# or looking for your own quieter space?
+
+
+# One can also do a dry run to set the default parameters
+# ini <- mice(impData, pred = pred, seed = 23109, maxit = 0)
+
+################################################################################
+# Trial imputation 2 (with 10 iterations, default is 5; ~15 mins)
+imp2 <- mice(impData, pred = pred, seed = 23109, maxit = 10)
+
+# Check for convergence
+par(mfrow = c(3, 1))
+plot(imp2, c(names(colnames(pred))))
+par(mfrow = c(1, 1))
+
+################################################################################
+# Trial imputation 3 (with 20 iterations, default is 5; ~35 mins)
+imp3 <- mice(impData, pred = pred, seed = 23109, maxit = 20, print = TRUE)
+
+# Check for convergence which look to be reasonable
+par(mfrow = c(3, 1))
+plot(imp3, c(names(colnames(pred))))
+par(mfrow = c(1, 1))
+
+imp3_set1 <- complete(imp3)
+imp3_set2 <- complete(imp3, 2)
+imp3_set3 <- complete(imp3, 3)
+imp3_set4 <- complete(imp3, 4)
+imp3_set5 <- complete(imp3, 5)
+
+write.csv(imp3_set1, "imp3_set1.csv")
+write.csv(imp3_set2, "imp3_set2.csv")
+write.csv(imp3_set3, "imp3_set3.csv")
+write.csv(imp3_set4, "imp3_set4.csv")
+write.csv(imp3_set5, "imp3_set5.csv")
+
+# Re-create the imputed train and test sets:
+# from imp1_set1
+train_imp1_set1 <- cbind(imp1_set1[1:nrow(train2016), ], train2016$Party)
+# names(train_imp1_set1)
+# all.equal(nrow(train_imp1_set1), nrow(train2016))
+test_imp1_set1 <- cbind(imp1_set1[(nrow(train2016)+1):nrow(imp1_set1), ])
+# names(test_imp1_set1)
+# all.equal(nrow(test_imp1_set1), nrow(test2016))
+write.csv(train_imp1_set1, "train_imp1_set1.csv")
+write.csv(test_imp1_set1, "test_imp1_set1.csv")
+
+
+# from imp3_set1 to set5
+train_imp3_set1 <- cbind(imp3_set1[1:nrow(train2016), ], train2016$Party)
+test_imp3_set1 <- cbind(imp3_set1[(nrow(train2016)+1):nrow(imp3_set1), ])
+
+train_imp3_set2 <- cbind(imp3_set2[1:nrow(train2016), ], train2016$Party)
+test_imp3_set2 <- cbind(imp3_set2[(nrow(train2016)+1):nrow(imp3_set2), ])
+
+train_imp3_set3 <- cbind(imp3_set3[1:nrow(train2016), ], train2016$Party)
+test_imp3_set3 <- cbind(imp3_set3[(nrow(train2016)+1):nrow(imp3_set3), ])
+
+train_imp3_set4 <- cbind(imp3_set4[1:nrow(train2016), ], train2016$Party)
+test_imp3_set4 <- cbind(imp3_set4[(nrow(train2016)+1):nrow(imp3_set4), ])
+
+train_imp3_set5 <- cbind(imp3_set5[1:nrow(train2016), ], train2016$Party)
+test_imp3_set5 <- cbind(imp3_set5[(nrow(train2016)+1):nrow(imp3_set5), ])
+
+write.csv(train_imp3_set1, "train_imp3_set1.csv")
+write.csv(test_imp3_set1, "test_imp3_set1.csv")
+
+write.csv(train_imp3_set2, "train_imp3_set2.csv")
+write.csv(test_imp3_set2, "test_imp3_set2.csv")
+
+write.csv(train_imp3_set3, "train_imp3_set3.csv")
+write.csv(test_imp3_set3, "test_imp3_set3.csv")
+
+write.csv(train_imp3_set4, "train_imp3_set4.csv")
+write.csv(test_imp3_set4, "test_imp3_set4.csv")
+
+write.csv(train_imp3_set5, "train_imp3_set5.csv")
+write.csv(test_imp3_set5, "test_imp3_set5.csv")
+################################################################################
+################################################################################
+
+
+
+
+# Preliminary data exploration
+
+
+
 str(trainMi)
 summary(trainMi)
 
--- question_selection.R
+++ question_selection.R
@@ -17,7 +17,8 @@ party_pairwise_acc <- vector()
 party_pairwise_ppv1 <- vector()
 party_pairwise_ppv2 <- vector()
 party_pairwise_ppv <- vector()
-party_pairwise_cut <- vector()
+party_pairwise_cut_05 <- vector()
+party_pairwise_cut_0005 <- vector()
 party_pairwise_pval <- vector()
 for(i in questions){
         resultsTable = table(trainChiSq$Party,trainChiSq[[i]])
@@ -28,20 +29,23 @@ for(i in questions){
         party_pairwise_ppv1[i] <- resultsTable[1,1]/(resultsTable[1,1]+resultsTable[2,1])
         party_pairwise_ppv2[i] <- resultsTable[1,2]/(resultsTable[1,2]+resultsTable[2,2])
         party_pairwise_ppv[i] <- party_pairwise_ppv1[i]/party_pairwise_ppv2[i]
-        party_pairwise_cut[i] <- x$p.value < 0.000001
+        party_pairwise_cut_05[i] <- x$p.value < 0.05
+        party_pairwise_cut_0005[i] <- x$p.value < 0.0005
         party_pairwise_pval[i] <- x$p.value
 }
 
 # Plot data to choose cut points for data
-hist(party_pairwise_pval, breaks = 100)
-
-hist(party_pairwise_acc_diag1, breaks = 20)
-hist(party_pairwise_acc_diag2, breaks = 20)
-hist(party_pairwise_acc, breaks = 20)
-
-hist(party_pairwise_ppv1, breaks = 20)
-hist(party_pairwise_ppv2, breaks = 20)
-hist(party_pairwise_ppv, breaks = 20)
+# hist(party_pairwise_pval, breaks = 100)
+# length(party_pairwise_pval[party_pairwise_pval < 0.05])
+# hist(-log(party_pairwise_pval), breaks = 100)
+# 
+# hist(party_pairwise_acc_diag1, breaks = 20)
+# hist(party_pairwise_acc_diag2, breaks = 20)
+# hist(party_pairwise_acc, breaks = 20)
+# 
+# hist(party_pairwise_ppv1, breaks = 20)
+# hist(party_pairwise_ppv2, breaks = 20)
+# hist(party_pairwise_ppv, breaks = 20)
 
 # boxplot(party_pairwise_acc_diag2 ~ party_pairwise_cut)
 
@@ -52,66 +56,76 @@ hist(party_pairwise_ppv, breaks = 20)
 # head(party_pairwise_cut, 20)
 # head(party_pairwise_pval, 20)
 
+# Proportion of questions included at each p value (lowered for multiple comparison)
+mean(party_pairwise_cut_05, na.rm = TRUE)
+mean(party_pairwise_cut_0005, na.rm = TRUE)
 
 # Review which type of selection to use: decided on ChiSq p value in the end
-seln_acc_diag1 <- which(party_pairwise_acc_diag1 < 0.44 | party_pairwise_acc_diag1 > 0.55)
-seln_acc_diag2 <- which(party_pairwise_acc_diag2 < 0.45 | party_pairwise_acc_diag2 > 0.56)
-seln_acc <- which(party_pairwise_acc < 0.7 | party_pairwise_acc > 1.3)
-
-seln_ppv1 <- which(party_pairwise_ppv1 < 0.45 | party_pairwise_ppv1 > 0.6)
-seln_ppv2 <- which(party_pairwise_ppv2 < 0.45 | party_pairwise_ppv2 > 0.65)
-seln_ppv <- which(party_pairwise_ppv < 0.8 | party_pairwise_ppv > 1.2)
+# seln_acc_diag1 <- which(party_pairwise_acc_diag1 < 0.44 | party_pairwise_acc_diag1 > 0.55)
+# seln_acc_diag2 <- which(party_pairwise_acc_diag2 < 0.45 | party_pairwise_acc_diag2 > 0.56)
+# seln_acc <- which(party_pairwise_acc < 0.7 | party_pairwise_acc > 1.3)
+# 
+# seln_ppv1 <- which(party_pairwise_ppv1 < 0.45 | party_pairwise_ppv1 > 0.6)
+# seln_ppv2 <- which(party_pairwise_ppv2 < 0.45 | party_pairwise_ppv2 > 0.65)
+# seln_ppv <- which(party_pairwise_ppv < 0.8 | party_pairwise_ppv > 1.2)
 
 # This contains all of the others:
-seln_cut <- which(party_pairwise_cut)
+seln_cut_0005 <- which(party_pairwise_cut_0005)
 
-sigQs <- names(trainChiSq)[seln_cut]
+sigQs <- names(trainChiSq)[seln_cut_0005]
 sigQs <- sigQs[!is.na(sigQs)]
 sigQs
 
 # Note that Q113181 and Q98197 are almost identical, but Q113181 has fewer NAs
-plot(jitter(as.numeric(trainChiSq$"Q113181"), 2), jitter(as.numeric(trainChiSq$"Q98197"), 2))
-sum(is.na(trainChiSq$"Q113181"))
-sum(is.na(trainChiSq$"Q98197"))
+# plot(jitter(as.numeric(trainChiSq$"Q113181"), 2), jitter(as.numeric(trainChiSq$"Q98197"), 2))
+# sum(is.na(trainChiSq$"Q113181"))
+# sum(is.na(trainChiSq$"Q98197"))
 
 sigQs <- sigQs[-which(sigQs == "Q98197")]
+write.csv(sigQs, file = "sigQs.csv")
 
+# Now select possible questions to exclude because unlikely associated with Party
+seln_cut_05 <- which(party_pairwise_cut_05)
 
+insigQs <- names(trainChiSq)[-seln_cut_05]
+insigQs <- insigQs[grep("^Q",insigQs)]
+insigQs
+write.csv(insigQs, file = "insigQs.csv")
 ################################################################################
 # Now repeat for higher threshold of cut (10 fold higher), ignoring other methods:
 
-party_pairwise_cut <- vector()
-party_pairwise_pval <- vector()
-for(i in questions){
-        resultsTable = table(trainChiSq$Party,trainChiSq[[i]])
-        x <- chisq.test( resultsTable)
-        party_pairwise_cut[i] <- x$p.value < 0.00001
-        party_pairwise_pval[i] <- x$p.value
-}
-seln_cut <- which(party_pairwise_cut)
-sigQs <- names(trainChiSq)[seln_cut]
-sigQs <- sigQs[!is.na(sigQs)]
-sigQs
-
-# This adds another two questions
-
-# Now repeat for higher threshold of cut (10 fold higher), ignoring other methods:
-party_pairwise_cut <- vector()
-party_pairwise_pval <- vector()
-for(i in questions){
-        resultsTable = table(trainChiSq$Party,trainChiSq[[i]])
-        x <- chisq.test( resultsTable)
-        party_pairwise_cut[i] <- x$p.value < 0.0001
-        party_pairwise_pval[i] <- x$p.value
-}
-seln_cut <- which(party_pairwise_cut)
-sigQs <- names(trainChiSq)[seln_cut]
-sigQs <- sigQs[!is.na(sigQs)]
-sigQs
-
-# This adds another three questions
-# Final list of questions associated with Party in univariate (ChiSq) analysis
-sigQs <- sigQs[-which(sigQs == "Q98197")]
+# party_pairwise_cut <- vector()
+# party_pairwise_pval <- vector()
+# for(i in questions){
+#         resultsTable = table(trainChiSq$Party,trainChiSq[[i]])
+#         x <- chisq.test( resultsTable)
+#         party_pairwise_cut[i] <- x$p.value < 0.00001
+#         party_pairwise_pval[i] <- x$p.value
+# }
+# seln_cut <- which(party_pairwise_cut)
+# sigQs <- names(trainChiSq)[seln_cut]
+# sigQs <- sigQs[!is.na(sigQs)]
+# sigQs
+# 
+# # This adds another two questions
+# 
+# # Now repeat for higher threshold of cut (10 fold higher), ignoring other methods:
+# party_pairwise_cut <- vector()
+# party_pairwise_pval <- vector()
+# for(i in questions){
+#         resultsTable = table(trainChiSq$Party,trainChiSq[[i]])
+#         x <- chisq.test( resultsTable)
+#         party_pairwise_cut[i] <- x$p.value < 0.0001
+#         party_pairwise_pval[i] <- x$p.value
+# }
+# seln_cut <- which(party_pairwise_cut)
+# sigQs <- names(trainChiSq)[seln_cut]
+# sigQs <- sigQs[!is.na(sigQs)]
+# sigQs
+# 
+# # This adds another three questions
+# # Final list of questions associated with Party in univariate (ChiSq) analysis
+# sigQs <- sigQs[-which(sigQs == "Q98197")]
 
 ################################################################################
 # Check each against the other for extreme correlation: doesn't appear to be any
@@ -128,15 +142,16 @@ for(i in 1:nrow(mat_sigQs)){
                              trainChiSq[ , as.character(mat_sigQs$Var2[i])], 
                              useNA = "no")
         x <- chisq.test( resultsTable)
-        output_pairwise_cut[[i]] <- x$p.value < 0.000001
+        output_pairwise_cut[[i]] <- x$p.value < 0.0005
         output_pairwise_pval[[i]] <- x$p.value
 }
 head(output_pairwise_cut)
 head(output_pairwise_pval)
 idx_p.val <- order(output_pairwise_pval)
 sigQPlots <- mat_sigQs[idx_p.val, ]
-# Remove first 12
-sigQPlots <- sigQPlots[13:144, ]
+
+# Remove first one's which are matched against themselves
+sigQPlots <- sigQPlots[(length(sigQs)+1):nrow(sigQPlots), ]
 # Take every second plot (duplicated)
 sigQPlots <- sigQPlots[seq(from = 1, to = nrow(sigQPlots), by = 2), ]
 
@@ -151,13 +166,13 @@ for(i in 1:nrow(sigQPlots)){
 }
 par(mfrow = c(1, 1))
 ################################################################################
-# Pairwise ChiSq between question variables
+################################################################################
+# Pairwise ChiSq between question variables (note more stringent p value)
 
 # Create dataframe for pairwise comparisons
 df_Qs2x2 <- expand.grid(names(trainChiSq)[questions], names(trainChiSq)[questions])
 # Remove identities
 df_Qs2x2 <- df_Qs2x2[-which(df_Qs2x2$Var1 == df_Qs2x2$Var2), ]
-
 
 Qs2x2_pairwise_cut <- vector()
 Qs2x2_pairwise_pval <- vector()
@@ -225,26 +240,62 @@ names(corr_Qs_list) <- names(trainChiSq)[questions]
 corr_Qs_list
 
 ################################################################################
+# Check that p values of last terms are reasonable (start with from = 15)
+lastTerm <- unlist(corr_Qs_list)[seq(from = 7, to = length(unlist(corr_Qs_list)), by = 15)]
+firstTerm <- names(corr_Qs_list)
+check <- data.frame(firstTerm, lastTerm)
+rownames(check) = NULL
+colnames(check) = c("Var1", "Var2")
+
+check_pairwise_pval <- vector()
+for(i in 1:nrow(check)){
+        resultsTable = table(trainChiSq[ , as.character(check$Var1[i])], 
+                             trainChiSq[ , as.character(check$Var2[i])])
+        x <- chisq.test( resultsTable)
+        check_pairwise_pval[[i]] <- x$p.value
+}
+mean(check_pairwise_pval > 0.05)
+which(check_pairwise_pval > 0.05)
+# Shows that 37 and 94 only have first 6 comparisions with p > 0.05
+# corr_Qs_list[37]
+# corr_Qs_list[94]
+
+# Could either set entries 7:15 to NULL or omit those items altogether
+# Probably better to create the dataframe first
+# corr_Qs_list_update <- corr_Qs_list
+# corr_Qs_list_update[[37]] <- corr_Qs_list_update[[37]][-c(7:15)]
+# corr_Qs_list_update[[94]] <- corr_Qs_list_update[[94]][-c(7:15)]
+# corr_Qs_list_update
+
+# Create dataframe of related questions for imputation
+imp_Qs <- as.data.frame(corr_Qs_list)
+cols <- 1:length(imp_Qs)
+imp_Qs[ , cols] <- apply(imp_Qs[, cols], 2, function(x) as.character(x))
+imp_Qs[7:15, c(37, 94)] <- NA
+
+write.csv(imp_Qs, file = "imputation_questions.csv")
+
+################################################################################
 ################################################################################
 
 
-test <- as.character(unlist(highCorPlots[corr_Qs[[1]][3], ]))
-names(trainChiSq)[7 + 1]
-corr_Qs[[1]]
-test <- test[test != names(trainChiSq)[7 + 1]]
-
-test <- lapply(corr_Qs, length)
-head(unlist(test), 1000)
-hist(unlist(test))
-highCorPlots[ 436, ]
-highCorPlots[4536, ]
-highCorPlots[4604, ]
-highCorPlots[4757, ]
-
-
-names(trainChiSq)[questions]
-seq_along(questions)
-as.character(highCorPlots[1, 1])
+# test <- as.character(unlist(highCorPlots[corr_Qs[[1]][3], ]))
+# names(trainChiSq)[7 + 1]
+# corr_Qs[[1]]
+# test <- test[test != names(trainChiSq)[7 + 1]]
+# 
+# test <- lapply(corr_Qs, length)
+# head(unlist(test), 1000)
+# hist(unlist(test))
+# highCorPlots[ 436, ]
+# highCorPlots[4536, ]
+# highCorPlots[4604, ]
+# highCorPlots[4757, ]
+# 
+# 
+# names(trainChiSq)[questions]
+# seq_along(questions)
+# as.character(highCorPlots[1, 1])
 # head(mat)
 # head(trainChiSq[ ,"Q124742"])
 # head(trainChiSq[ , as.character(mat$Var1)[1]])
@@ -268,70 +319,96 @@ as.character(highCorPlots[1, 1])
 # head(paste0(mat$Var1, mat$Var2))
 # class(output_pairwise_cut)
 
-# create matrix with the pairwise correlated questions
-mat_pairwise <- matrix(output_pairwise_cut, length(questions), length(questions))
 
-# as.numeric(mat_pairwise)
-# length(mat_pairwise)
-# dim(mat_pairwise)
-rownames(mat_pairwise) <- names(trainChiSq[questions])
-colnames(mat_pairwise) <- names(trainChiSq[questions])
+################################################################################
+# create matrix with the pairwise correlated questions
+# use df_Qs which includes identity matches (ie diagonals))
+# re-run the ChiSq test using a variable p value (previously 0.000001 for sigQs)
+df_Qs <- expand.grid(names(trainChiSq)[questions], names(trainChiSq)[questions])
+
+Qgraph2x2_pairwise_cut_000001 <- vector()
+Qgraph2x2_pairwise_cut_0001 <- vector()
+Qgraph2x2_pairwise_cut_01 <- vector()
+for(i in 1:nrow(df_Qs2x2)){
+        resultsTable = table(trainChiSq[ , as.character(df_Qs$Var1[i])], 
+                             trainChiSq[ , as.character(df_Qs$Var2[i])], 
+                             useNA = "no")
+        x <- chisq.test( resultsTable)
+        Qgraph2x2_pairwise_cut_000001[[i]] <- x$p.value < 0.000001
+        Qgraph2x2_pairwise_cut_0001[[i]] <- x$p.value < 0.0001
+        Qgraph2x2_pairwise_cut_01[[i]] <- x$p.value < 0.01
+}
+
+cut_000001 <- Qgraph2x2_pairwise_cut_000001
+cut_0001 <- Qgraph2x2_pairwise_cut_0001
+cut_01 <- Qgraph2x2_pairwise_cut_01
+
+mat_pairwiseQ <- matrix(cut_000001, length(questions), length(questions))
+
+rownames(mat_pairwiseQ) <- names(trainChiSq[questions])
+colnames(mat_pairwiseQ) <- names(trainChiSq[questions])
 
 # mat_pairwise[!lower.tri(mat_pairwise)] <- 0  # unnecessary if parameters set below
 # sum(mat_pairwise)
 
 
-# create graph showing the correlations (edges) between questions (vertices)
-library(igraph)
+# create graph showing the relations (edges) between correlated questions (vertices)
+# library(igraph)
 # ?igraph
 # take upper triangular matrix and exclude diagonal to avoid repeat/identity matches
-pairwise <- graph_from_adjacency_matrix(adjmatrix = mat_pairwise, 
+pairwiseQ <- graph_from_adjacency_matrix(adjmatrix = mat_pairwiseQ, 
                                         mode = "upper", 
                                         diag = FALSE,
                                         add.colnames = NULL, add.rownames = NA)
-# print(pairwise)
-# str(pairwise)
+# print(pairwiseQ)
+# str(pairwiseQ)
 
 # make size proportional to the degree of each vertex
-V(pairwise)$size = degree(pairwise)/2+2
-plot(pairwise, vertex.label=NA)
+par(mfrow = c(1, 1))
+V(pairwiseQ)$size = degree(pairwiseQ)
+plot(pairwiseQ, vertex.label=NA)
 
 # How many vertices linked to more than x others
-# sum(degree(pairwise) > 20)
-# sum(degree(pairwise) > 50)
-# max(degree(pairwise))
-# min(degree(pairwise))
+hist(degree(pairwiseQ))
 
 
-no_corr_Q <- which(degree(pairwise) == 0)
-lo_corr_Q <- which(degree(pairwise) < 10)
-hi_corr_Q <- which(degree(pairwise) > 20)
 
-# Ensure no overlap between groups
-# sort(names(no_corr_Q))
-# sort(names(lo_corr_Q))
-# sort(names(hi_corr_Q))
-# names(no_corr_Q) %in% names(lo_corr_Q)
-# names(lo_corr_Q) %in% names(hi_corr_Q)        
+no_corr_Q <- which(degree(pairwiseQ) == 0)
+lo_corr_Q <- which(degree(pairwiseQ) < 10)
+hi_corr_Q <- which(degree(pairwiseQ) > 20)
 
+###############################################################################
+# Read in question list
+library(xlsx)
+question_list <- read.xlsx("Questions.xlsx", sheetIndex = 1, colIndex = 2:3)
+# Index question list by corresponding column in train
+question_list$idx <- match(question_list$Question_ID, names(trainChiSq))
 
-# names(no_corr_Q) %in% sigQs
-# names(lo_corr_Q) %in% sigQs
-# names(hi_corr_Q) %in% sigQs
+# High degree questions
+Q_hi_corr <- hi_corr_Q[names(hi_corr_Q)]
+question_list[question_list$Question_ID %in% names(Q_hi_corr), 1:2]
+Q_hi_degree <- as.character(question_list[question_list$Question_ID %in% names(Q_hi_corr), 1])
 
-# High degree questions also correlated with outcome
+write.csv(Q_hi_degree, file = "Q_hi_degree.csv")
+
+# High degree questions also correlated with outcome (Party)
 a <- hi_corr_Q[names(hi_corr_Q) %in% sigQs]
 question_list[question_list$Question_ID %in% names(a), 1:2]
 
-# High degree questions not correlated with outcome
-b <- hi_corr_Q[!c(names(hi_corr_Q) %in% sigQs)]
-question_list[question_list$Question_ID %in% names(b), 1:2]
+# Significant questions correlated with outcome (Party)
+question_list[question_list$Question_ID %in% sigQs, 1:2]
 
-# Low degree questions also correlated with outcome
-c <- lo_corr_Q[c(names(lo_corr_Q) %in% sigQs)]
-question_list[question_list$Question_ID %in% names(c), 1:2]
+# Insignificant questions unlikely correlated with outcome (Party)
+question_list[question_list$Question_ID %in% insigQs, 1:2]
 
-# Low degree questions not correlated with outcome
-d <- lo_corr_Q[!c(names(lo_corr_Q) %in% sigQs)]
-question_list[question_list$Question_ID %in% names(d), 1:2]
 
+
+
+# Examine modified matrix with correlated questions for imputation
+imp_q_matrix <- read.csv("imp_q_file.csv", header = TRUE)
+head(imp_q_matrix)
+
+
+
+################################################################################
+################################################################################
